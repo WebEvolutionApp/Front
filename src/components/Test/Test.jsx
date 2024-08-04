6cb4ce706b53dd8b709a21c16c70cc8585fe7e6f
Katsiaryna Hanchar <kattie@outlook.com>
2024-08-04 21:34:03 +0200

    Initialized a react application

---
+++ src/components/Test/Test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from './styles.module.scss'
+
+export function Test() {
+    return(
+        <div className={s.style}>
+            webEVOLUTION!!!
+        </div>
+    )
+}
+export default Test;
