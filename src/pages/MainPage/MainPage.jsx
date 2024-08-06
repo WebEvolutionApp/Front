8f4137b7295cb2bf46d0e7acd0551d66f9b41b05
Katsiaryna Hanchar <kattie@outlook.com>
2024-08-06 18:12:30 +0200

    OurServeces in progress

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import s from './styles.module.scss';
 import ReasonsToWorkWithUs from "../../components/ReasonsToWorkWithUs/ReasonsToWorkWithUs";
+import OurServices from "../../components/OurServices/OurServices";
 
 export const MainPage = () => {
 return (
     <>
 <ReasonsToWorkWithUs />
+<OurServices />
     </>
 )
 }
