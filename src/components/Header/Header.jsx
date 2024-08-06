0fd14b5105fe528bc4d516f72a967072ee2d1719
Danila <emelchenko1997@yandex.ru>
2024-08-06 21:27:29 +0300

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import s from './Header.module.scss';
+
+function Header() {
+  return (
+    <div className={s['container']}>
+      <div className={s['content']}>
+        <img className={s['header-logo']} src="./icons/logo-web-evolution.svg" alt="Logo Web Evolution" />
+
+        <nav className={s['navbar']}>
+          <ul className={s['nav-menu']}>
+            <li className={s['nav-item']}><a className={s['nav-link']} href="#">Этапы работы</a></li>
+            <li className={s['nav-item']}><a className={s['nav-link']} href="#">Услуги</a></li>
+            <li className={s['nav-item']}><a className={s['nav-link']} href="#">О нас</a></li>
+            <li className={s['nav-item']}><a className={s['nav-link']} href="#">Контакты</a></li>
+          </ul>
+        </nav>
+
+        <div className={s['header-socials']}>
+          <a href="#"><img src="./icons/telegram.svg" alt="Telegram" /></a>
+          <a href="#"><img src="./icons/instagram.svg" alt="Instagram" /></a>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+
 import s from './styles.module.scss';
 
-export const MainPage = () => {
-return (
-    <>
+import Header from '../../components/Header/Header';
+import HeroSection from '../../components/HeroSection/HeroSection';
 
+export const MainPage = () => {
+  return (
+    <>
+      <Header />
     </>
-)
+  )
 }
 export default MainPage;
