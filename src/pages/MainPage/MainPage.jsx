54b0264774ba7f77c828f62486ce47238c677b47
Danila <emelchenko1997@yandex.ru>
2024-08-06 21:36:58 +0300

    header

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import s from './styles.module.scss';
 
 import Header from '../../components/Header/Header';
-import HeroSection from '../../components/HeroSection/HeroSection';
 
 export const MainPage = () => {
   return (
