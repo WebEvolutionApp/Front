ddaa114de6d03d07d05e966f67fb9997ebf05b30
Danila <emelchenko1997@yandex.ru>
2024-08-06 22:02:36 +0300

    Header

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+
 import s from './styles.module.scss';
+
 import ReasonsToWorkWithUs from "../../components/ReasonsToWorkWithUs/ReasonsToWorkWithUs";
 import OurServices from "../../components/OurServices/OurServices";
+import Header from '../../components/Header/Header';
 
 export const MainPage = () => {
-return (
+  return (
     <>
-<ReasonsToWorkWithUs />
-<OurServices />
+      <Header />
+      <ReasonsToWorkWithUs />
+      <OurServices />
     </>
-)
+  )
 }
 export default MainPage;
