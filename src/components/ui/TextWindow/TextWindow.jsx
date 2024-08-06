ffbbb6a1f91adf4cbdd97fdbb76c9c8cd6e14883
Katsiaryna Hanchar <kattie@outlook.com>
2024-08-06 18:12:30 +0200

    OurServeces in progress

--- src/components/ui/TextWindow/TextWindow.jsx
+++ src/components/ui/TextWindow/TextWindow.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import s from './styles.module.scss';
 import classNames from 'classnames';
 
-const TextWindow = ({ onHover, windowText, customClassName }) => {
+const TextWindow = ({ onHover, windowText, customClassName, width }) => {
   return (
     <div
       className={classNames(s.custom__textWindow, customClassName)}
       onMouseOver={onHover}
+      style={{ width }}
     >
       {windowText}
     </div>
