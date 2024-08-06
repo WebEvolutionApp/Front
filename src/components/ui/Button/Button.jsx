a494d01ff5cfdbbe8bb3e88e5904427f92918e76
Katsiaryna Hanchar <kattie@outlook.com>
2024-08-06 22:39:34 +0200

    styles tuned

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -4,7 +4,7 @@ function Button({ text }) {
   return (
     <div className={s['button']}>
       <p className={s['button-text']}>{text}</p>
-      <img src="./icons/arrow-right.svg" alt="arrow right" />
+      <img className={s['button__img']} src="./icons/arrow-right.svg" alt="arrow right" />
     </div>
   );
 }
