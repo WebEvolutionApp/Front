eb30d34ad2f14f26e06148c3a42d5500e90cc427
Katsiaryna Hanchar <kattie@outlook.com>
2024-08-11 12:25:04 +0200

    styles tuned at form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,35 +1,59 @@
 import React from "react";
 import s from "./styles.module.scss";
 import Input from "../ui/Input/Input";
+import FormButton from "../ui/FormButton/FormButton";
+import ellipseOne from "../../assets/images/Ellipse3.png";
+import ellipseTwo from "../../assets/images/Ellipse2.png";
 
 export function Form() {
-    return(
-        <div className={s.form__container}> 
-        <div className={s.form__content}>
+  return (
+    <div className={s.form__container}>
+      <img className={s.form__container__BgOne} src={ellipseOne} alt="" />
+      <img className={s.form__container__BgTwo} src={ellipseTwo} alt="" />
+      <img className={s.form__container__BgThree} src={ellipseOne} alt="" />
+      <img className={s.form__container__BgFour} src={ellipseTwo} alt="" />
+      <div className={s.form__content}>
         <div className={s.form__title}>Обратная связь</div>
         <div className={s.form__formWindow}>
-            <div className={s.form__formTitle}>Оставить заявку на консультацию по созданию сайта</div>
-            <div className={s.form__inputsSection}>
-                <div className={s.form__userInfo}>
-                    <Input placeholder = 'Имя*' customClassName={s.input} width="25vw" height={"6vw"} />
-                    <Input placeholder = 'E-mail*' customClassName={s.input} width="25vw" height={"6vw"} />
-                </div>
-                <Input placeholder = 'Опишите вашу идею' customClassName={s.input} width="38vw" height={"14.5vw"} />
-            </div>
-            <div className={s.form__requiredFields}>* поля обязательные для заполнения</div>
-            <div className={s.form__checkboxSection}>
-            <label className={s.checkboxLabel}>
-              <input
-                type="checkbox"
-                className={s.checkbox}
+          <div className={s.form__formTitle}>
+            Оставить заявку на консультацию по созданию сайта
+          </div>
+          <div className={s.form__inputsSection}>
+            <div className={s.form__userInfo}>
+              <Input
+                placeholder="Имя*"
+                customClassName={s.input}
+                width="25vw"
+                height={"6vw"}
               />
-             Согласие на обработку персональных данных
+              <Input
+                placeholder="E-mail*"
+                customClassName={s.input}
+                width="25vw"
+                height={"6vw"}
+              />
+            </div>
+            <Input
+              placeholder="Опишите вашу идею"
+              customClassName={s.input}
+              width="38vw"
+              height={"14.5vw"}
+            />
+          </div>
+          <div className={s.form__requiredFields}>
+            * поля обязательные для заполнения
+          </div>
+          <div className={s.form__checkboxSection}>
+            <label className={s.checkboxLabel}>
+              <input type="checkbox" className={s.checkbox} />
+              Согласие на обработку персональных данных
             </label>
           </div>
+          <FormButton customClassName={s.formBtn}>Отправить</FormButton>
         </div>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Form;
