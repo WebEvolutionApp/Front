fabf6a0db153f177fee6d3df946a21e50ee454cd
Katsiaryna Hanchar <kattie@outlook.com>
2024-08-15 10:47:31 +0200

    commit before switching

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,9 +7,7 @@ function Header() {
       <div className={s['content']}>
         <a className={s['header-logo-link']} href="#"><img className={s['header-logo']} src="./icons/logo-web-evolution.svg" alt="Logo Web Evolution" /></a>
 
-        <nav className={s['navbar']}>
           <Links menuStyle={s.headerNavbar__Menu} /> 
-        </nav>
 
         <div className={s['header-socials']}>
           <a href="#"><img className={s['header-socials-img']} src="./icons/telegram.svg" alt="Telegram" /></a>
